7796678fbe353690dafbe66434a0243c9514a4a9
unknown <oladejofavourajoke@gmail.com>
2022-08-09 16:01:54 +0100

    display area fix

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="container">
       <div className="grid">
+        <div className="dis"></div>
         <div className="padButton AC">AC</div>
         <div className="padButton C">C</div>
         <div className="padButton div">/</div>
@@ -20,10 +21,11 @@ function App() {
         <div className="padButton one">1</div>
         <div className="padButton two">2</div>
         <div className="padButton three">3</div>
-        <div className="padButton equals">=</div>
+        <div className="padButton equal">=</div>
         <div className="padButton zero">0</div>
         <div className="padButton dot">.</div>
-      </div>
+        
+        </div>
     </div>
   );
 }
