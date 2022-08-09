9ef51b06828231623f1ca3e1115219f4c87c9ad1
unknown <oladejofavourajoke@gmail.com>
2022-08-09 16:33:38 +0100

    operator styling

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ function App() {
   return (
     <div className="container">
       <div className="grid">
-        <div className="dis"></div>
-        <div className="padButton AC">AC</div>
-        <div className="padButton C">C</div>
-        <div className="padButton div">/</div>
-        <div className="padButton times">*</div>
-        <div className="padButton seven">7</div>
-        <div className="padButton eight">8</div>
-        <div className="padButton nine">9</div>
-        <div className="padButton minus">-</div>
-        <div className="padButton four">4</div>
-        <div className="padButton five">5</div>
-        <div className="padButton six">6</div>
-        <div className="padButton plus">+</div>
-        <div className="padButton one">1</div>
-        <div className="padButton two">2</div>
-        <div className="padButton three">3</div>
-        <div className="padButton equal">=</div>
-        <div className="padButton zero">0</div>
-        <div className="padButton dot">.</div>
+        <div className="dis "></div>
+        <div className="padButton AC orange">AC</div>
+        <div className="padButton C orange">C</div>
+        <div className="padButton div operator">/</div>
+        <div className="padButton times operator">*</div>
+        <div className="padButton seven dark-gray">7</div>
+        <div className="padButton eight dark-gray">8</div>
+        <div className="padButton nine dark-gray">9</div>
+        <div className="padButton minus operator">-</div>
+        <div className="padButton four dark-gray">4</div>
+        <div className="padButton five dark-gray">5</div>
+        <div className="padButton six dark-gray">6</div>
+        <div className="padButton plus operator">+</div>
+        <div className="padButton one dark-gray">1</div>
+        <div className="padButton two dark-gray">2</div>
+        <div className="padButton three dark-gray">3</div>
+        <div className="padButton equal operator">=</div>
+        <div className="padButton zero dark-gray">0</div>
+        <div className="padButton dot operator">.</div>
         
         </div>
     </div>
